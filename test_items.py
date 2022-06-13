1ddb35b2123ce2ce8a094277619d45a689f9847a
Rasul Mamedov <rasuulmamedov@gmail.com>
2022-06-13 20:29:01 +0300

    Changed sleep time

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
